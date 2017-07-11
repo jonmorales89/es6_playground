76cde79cef8f856b2c9a82a8b18f253608129c8d
jonmorales89 <jon.sal.morales@gmail.com>
2017-07-11 16:04:27 -0700

    superclass expample

--- app/index.js
+++ app/index.js
@@ -1,1 +1,40 @@
-console.log('App working: open app/index.js to start exploring ES6');
+class Greet {
+    constructor(name){
+        this.name = name;
+    }
+
+    sayHello(){
+        return `Hello ${this.name}`;
+    }
+
+    sayGoodbye(){
+        return`Goodbye ${this.name}`;
+    }
+}
+
+class BetterGreeting extends Greet {
+    constructor(name, lname){
+        super(name);
+        this.lastName = lname;
+    }
+    alertHello(){
+        alert(`What up, ${this.name} ${this.lastName}`);
+    }
+    sayHello(){
+        return super.sayHello() + ` ${this.lastName}`;
+    }
+}
+
+const jonathan = new Greet('Jonathan');
+const dave = new Greet('Dave');
+
+const bob = new BetterGreeting('Bob', 'Peters');
+
+console.log(jonathan.sayHello());
+console.log(jonathan.sayGoodbye());
+
+console.log(dave.sayHello());
+console.log(dave.sayGoodbye());
+
+bob.alertHello();
+console.log(bob.sayHello());
